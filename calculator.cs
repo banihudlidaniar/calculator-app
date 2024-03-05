4108d00ce13e102fa71821a0ffbd57f19ff5d195
banihudlidaniar <banihudlidaniar@students.amikom.ac.id>
2024-03-05 10:25:25 +0700

    Melengkapi Fungsi

--- calculator.cs
+++ calculator.cs
@@ -4,6 +4,12 @@ class Calculator
 	{
 		int a = 10;
 		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	
 	static int Penambahan(int a, int b)
