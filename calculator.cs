bb5b80e5f92d6bafd71ad94ecad68718c08c3e98
banihudlidaniar <banihudlidaniar@students.amikom.ac.id>
2024-03-05 10:22:36 +0700

    Pengurangan

--- calculator.cs
+++ calculator.cs
@@ -4,13 +4,14 @@ class Calculator
 	{
 		int a = 10;
 		int b = 6;
-		
-		
 	}
 	
 	static int Penambahan(int a, int b)
 	{
 		return a + b;
 	}
-
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
 }
